18693f1f3097ff41123772a4dde23be6001dc77c
Carsten Hennig <carsten.hennig@gmail.com>
2023-08-11 17:30:32 +0200

    basic function

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let map;
 let history = [];
 
 function initMap(lat, lng) {
-    map = L.map(mapDiv).setView([51.505, -0.09], 15);
+    map = L.map(mapDiv).setView([lat, lng], 15);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
     L.marker([lat, lng]).addTo(map);
 }
@@ -20,16 +20,16 @@ function updateHistoryList() {
         historyList.appendChild(li);
     });
 }
-
 searchButton.addEventListener('click', () => {
+    console.log("searchButton was clicked");
     const address = addressInput.value;
 
     // You would use a geocoding service API here to get latitude and longitude
     // For simplicity, let's assume you have the lat/lng already.
-    const lat = 51.505; // Example latitude
-    const lng = -0.09; // Example longitude
-    console.log("Coordinates are: ", lat, lng);
-    initMap(53.57106, 9.96518);
+    const lat = 51.500620; // Example latitude
+    const lng = 7.522520; // Example longitude
+
+    initMap(lat, lng);
 
     history.push(address);
     updateHistoryList();
